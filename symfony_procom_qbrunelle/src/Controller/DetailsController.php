0fe6b183f51f82782c135b2b919b8d4f12f2fada
Quentin BRUNELLE <quentin.brunelle@gmail.com>
2019-04-09 11:34:58 +0200

    Mise en place du README.md / Clean code DetailsController (detailProjet / livraisonProjet) / Enlever image email projet

--- symfony_procom_qbrunelle/src/Controller/DetailsController.php
+++ symfony_procom_qbrunelle/src/Controller/DetailsController.php
@@ -43,20 +43,22 @@ class DetailsController extends AbstractController
      */
     public function detailsProjet(Request $request, int $id, int $offset, bool $erreur = false)
     {
-        $projet = $this->projetRepository->find($id);
-        $employes = $this->employeRepository->findBy(['archivage' => 0]);
+        $projet = $this->projetRepository->find($id); // On récupère le projet pour lequel on souhaite obtenir des détails
 
+        // historique des temps de production par date décroissante et par tranche de 10
         $historique = $this->tempsDeProductionRepository->findBy(['projet' => $id],['dateSaisie' => 'DESC'], 10, $offset);
 
-        $coutTotalProjet = $this->tempsDeProductionRepository->findCoutTotalProjet($id);
-        $nbEmployes = $this->tempsDeProductionRepository->findEmployesByProject($id);
-
+        $coutTotalProjet = $this->tempsDeProductionRepository->findCoutTotalProjet($id); // Query : coût total du projet
         $coutTotal = $coutTotalProjet[0]['coutTotal'] == null ? 0 : $coutTotalProjet[0]['coutTotal'];
+        $nbEmployes = $this->tempsDeProductionRepository->findEmployesByProject($id); // Query : nombre d'employés affectés au projet
 
+        // Nombre de pages nécessaires pour l'affichage de tous les temps de production par tranche de 10 résultats
         $nb_pages = ceil(count($this->tempsDeProductionRepository->findBy(['projet' => $id])) / 10);
         $nb_pages = $nb_pages < 1 ? 1 : $nb_pages;
-        $current_page = ($offset /10) + 1;
 
+        $current_page = ($offset /10) + 1; // Page actuelle où se situe l'utilisateur
+
+        // $btns détermine si les chevrons sont désactivés ou non
         if($current_page == 1){
             $btns = ['disabled',''];
         }else if($current_page == $nb_pages){
@@ -65,37 +67,33 @@ class DetailsController extends AbstractController
             $btns = ['', ''];
         }
 
-        $chest = [
-            'title' => $projet->getIntitule(),
-            'icon' => 'laptop',
-            'active' => ["dashboard" => "", "projets" => "active", "employes" => "", "metiers" => "" ]
-        ];
+        /* FORMULAIRE */
 
-        /*
-         *  FORMULAIRE
-         */
-
+        // Création d'un nouveau temps de production et affectation du projet sur lequel on se trouve
         $newTime = new TempsProductionEmployeProjet();
         $newTime->setProjet($projet);
 
-        /**
-         * On récupère la concaténation "NOM Prénom" afin de l'afficher dans le select (les clés seulement étant affichées)
-         * On injecte ensuite un employé en tant que valeu (car la valeur attendue en retour du formulaire est un employé)
-         */
+        // Choix de l'entité pour laquelle le formulaire va récupérer l'id (dans le cas d'un projet, il faut récupérer
+        // l'employé). Utilisé dans la création du formulaire dans le template
+        $entity_form = 'employe';
 
+        $employes = $this->employeRepository->findBy(['archivage' => 0]); // Liste des employés non archivés
+
+        // On récupère la concaténation "NOM Prénom" afin de l'afficher dans le select (les clés seulement étant affichées)
+        // On injecte ensuite un employé en tant que valeur (car la valeur attendue en retour du formulaire est un employé)
         for($i = 0; $i < sizeof($employes); $i++){
             $label = strtoupper($employes[$i]->getNom()).' '.$employes[$i]->getPrenom();
             $liste[$label] = $employes[$i];
         }
 
-        $entity_form = 'employe'; // Choix de l'entité pour laquelle le formulaire va récupérer l'id
-
+        // Création du formulaire et ajout d'un champ de type 'select' utilisant la liste des employés non archivés
         $form = $this->createForm(AddProductionTimeType::class, $newTime)->add($entity_form, ChoiceType::class,[
             'label' => 'Employés',
             'choices' => $liste
         ]);
         $form->handleRequest($request);
 
+        // Validation du formulaire
         if($form->isSubmitted() && $form->isValid()){
             $newTime->setCoutTotal($newTime->getEmploye()->getCoutJournalier() * $newTime->getDuree());
 
@@ -104,6 +102,12 @@ class DetailsController extends AbstractController
 
             return $this->redirectToRoute('details_projet',['id' => $id, 'offset' => 0]);
         }
+
+        $chest = [
+            'title' => $projet->getIntitule(),
+            'icon' => 'laptop',
+            'active' => ["dashboard" => "", "projets" => "active", "employes" => "", "metiers" => "" ]
+        ];
 
         return $this->render('details/detail.html.twig', [
             'type_detail' => 'projet',
@@ -127,6 +131,7 @@ class DetailsController extends AbstractController
      */
 
     public function livraisonProjet(Request $request, int $id){
+        // Récupération du projet puis set la livraison à true
         $projet = $this->projetRepository->find($id);
         $projet->setEstLivre(1);
         $this->em->persist($projet);
